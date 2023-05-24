d72562f8d0c94c4565085577c7284074e5ec0d3b
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2023-05-24 10:07:17 +0300

    Migrated to turbo

--- packages/customer-fe/src/app/cart-button.tsx
+++ packages/customer-fe/src/app/cart-button.tsx
@@ -9,7 +9,7 @@ import { Button } from '~/components/ui/button';
 export default function CartButton() {
     return (
         <Sheet>
-            <SheetTrigger>
+            <SheetTrigger asChild>
                 <Button
                     variant="outline"
                     className="text-foreground/80"
