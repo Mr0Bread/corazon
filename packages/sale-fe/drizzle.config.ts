3c833a09715fcd3ea63600b14a3190b127d22ecc
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2023-06-01 23:38:39 +0300

    Removed drizzle config from git

--- packages/sale-fe/drizzle.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from 'drizzle-kit';
-
-export default {
-    schema: './packages/sale-fe/src/server/schema.ts',
-    connectionString: 'mysql://qtja0olgc2m06g8b44cj:pscale_pw_4EBsSr8hWo0G0RkNL0FWnf3NAdtHR2qLBYhKl0qpoE9@aws.connect.psdb.cloud/corazon?ssl={"rejectUnauthorized":true}'
-} satisfies Config;
